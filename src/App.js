cfe1cb8b9378c0074e8c493ba039562ff4c5e7ca
Akindunjoye <tolulope@busha.co>
2025-01-01 00:16:46 +0100

    rockview

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Create from './Create';
-import BlogDetails from './BlogDetails';
-import NotFound from './NotFound';
+import Landing from "./components/Landing";
 
 function App() {
   return (
-    <Router>
-        <div className="App">
-      <Navbar />
-        <div className='content'>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path="/blogdetails/:param">
-              <BlogDetails/>
-            </Route>
-            <Route path="*">
-              <NotFound/>
-            </Route>
-          </Switch>
-        </div>
-        </div>
-    </Router> 
+    <div>
+      <Landing />
+    </div>
   );
 }
 
